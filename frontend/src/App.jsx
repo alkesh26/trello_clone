8bc1b9f52085f07111e31ff1f94510505eab87ee
Alkesh Ghorpade <alkesh26@gmail.com>
2025-07-08 17:35:19 +0530

    Added vite + react and added a test Cards component

---
+++ frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Cards from '../components/Cards'
+
+function App() {
+
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Cards />
+    </>
+  )
+}
+
+export default App
