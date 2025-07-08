5e36e13b3c1af6f4cb9d0569fda3ea00830bff7c
Alkesh Ghorpade <alkesh26@gmail.com>
2025-07-08 13:36:49 +0530

    Changed /cards to /api/vi/cards and fixed specs

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :cards
+  scope module: 'api/v1', path: 'api/v1', as: '' do
+    resources :cards
+  end
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
