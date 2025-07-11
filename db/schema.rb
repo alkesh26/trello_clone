558e6abbb7bf41a1d216a4f3189602c194d497b9
Alkesh Ghorpade <alkesh26@gmail.com>
2025-07-11 23:57:13 +0530

    Corrected rubocop issues

--- db/schema.rb
+++ db/schema.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -10,13 +12,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_07_11_173239) do
-  create_table "cards", force: :cascade do |t|
-    t.string "title"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "status"
+ActiveRecord::Schema[7.1].define(version: 20_250_711_173_239) do
+  create_table 'cards', force: :cascade do |t|
+    t.string 'title'
+    t.string 'description'
+    t.datetime 'created_at', null: false
+    t.datetime 'updated_at', null: false
+    t.string 'status'
   end
-
 end
