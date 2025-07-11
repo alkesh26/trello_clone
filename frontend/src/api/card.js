b927bf36e85de0504e5707b17478b69f365011e3
Alkesh Ghorpade <alkesh26@gmail.com>
2025-07-11 23:59:54 +0530

    Added card API wrapper

---
+++ frontend/src/api/card.js
@@ -0,0 +1,11 @@
+import api from './client';
+
+export async function createCardAPI(body) {
+  try {
+    const response = await api.post('/cards', { card: body });
+    return response.data;
+  } catch (error) {
+    // add a sticky note to display the error
+    return null;
+  }
+}
